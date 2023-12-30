4fe4e9f6b5e9c140f26252ca23cecc273258a49a
Rahomotul Islam (Shopnil) <shopnilkhan766@gmail.com>
2023-12-30 23:58:28 +0600

    Update good_bye_2023_A.cpp

--- Contests/good_bye_2023_A.cpp
+++ Contests/good_bye_2023_A.cpp
@@ -2,49 +2,6 @@
 using namespace std;
 typedef long long ll;
 #define f(i,n) for(int i=0;i<n;i++)
-void test()
-{
-       /*     ll n,k,mult=1;
-        cin>>n>>k;
-        bool check =false;
-        f(i,n)
-        {
-            ll b;cin>>b;
-            mult*=b;
-            if(2023%mult!=0)
-            {
-                check=true;
-            }
-        }
-        if(check)
-        {
-            cout<<"NO\n";
-            return;
-        }
-        cout<<"YES\n";
-        cout<<2023/mult;
-        for(int i=0;i<k-1;i++)
-        {
-            cout<<" "<<1;
-        }
-        cout<<"\n";*/
-        ll n,k;
-        cin>>n>>k;
-        vector<ll>v(n);
-        f(i,n)cin>>v[i];
-        ll mult=1;
-        f(i,n)
-        mult*=v[i];
-        ll x=2023/mult;
-        if(mult*x==2023)
-        {
-            cout<<"YES\n";
-            f(i,k-1)
-            cout<<1<<" ";
-            cout<<x<<endl;
-        }
-        else
-            cout<<"NO\n";
 
 }
 int main()
@@ -52,11 +9,32 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
+    ll t;
     cin>>t;
     while(t--)
     {
-        test();
+        ll n,k;
+        cin>>n>>k;
+        ll multi=1;
+        while(n--)
+        {
+            ll x;
+            cin>>x;
+            multi*=x;
+        }
+        if(2023%multi)
+        {
+            cout<<"NO\n";
+            continue;
+        }
+        cout<<"YES\n";
+        cout<<2023/multi<<" ";
+        k--;
+        while(k--)
+
+            cout<<1<<" ";
+            cout<<endl;
+
     }
     return 0;
 }
